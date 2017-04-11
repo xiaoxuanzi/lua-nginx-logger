d7b8b89dcfe93aae716d0168f3a0f1985780b677
xiaoxuanzi <xiaoximou@gmail.com>
2017-04-11 15:18:40 +0800

    fix logger

--- logger.lua
+++ logger.lua
@@ -2,22 +2,32 @@ local file_ffi = require "std_ffi.file"
 
 local _M = {}
 
+local function transform( var )
+
+    if var == nil then
+        return '-'
+    end
+
+    return var
+
+end
+
 local function access_log()
 
-    local msg = 
-        'timestamp:'       .. ngx.var.time_local .. ',' ..
-        'remote_addr:'     .. ngx.var.remote_addr .. ',' ..
-        'remote_user:'     .. ngx.var.remote_user .. ',' ..
-        'upstream_addr:'   .. ngx.var.upstream_addr .. ',' ..
-        'status:'          .. ngx.var.status .. ',' ..
-        'body_bytes_sent:' .. ngx.var.body_bytes_sent .. ',' ..
-        'request:'         .. ngx.var.request .. ',' ..
-        'http_user_agent:' .. ngx.var.http_user_agent .. ',' ..
-        'request_time:'    .. ngx.var.request_time .. ',' ..
-        'http_referer:'    .. ngx.var.http_referer .. ',' ..
-        'http_x_forwarded_for:' .. ngx.var.http_x_forwarded_for .. ',' ..
-        'server_name:'     .. ngx.var.server_name .. ',' ..
-        'bytes_sent:'      .. ngx.var.bytes_sent .. ','
+    local msg = '{ ' ..
+        '\"timestamp\":\"'       .. ngx.var.time_local .. ',' ..
+        '\"remote_addr\":\"'     .. ngx.var.remote_addr .. ',' ..
+        '\"remote_user\":\"'     .. transform(ngx.var.remote_user) .. ',' ..
+        '\"upstream_addr\":\"'   .. transform(ngx.var.upstream_addr) .. ',' ..
+        '\"status\":\"'          .. ngx.var.status .. ',' ..
+        '\"body_bytes_sent\":\"' .. ngx.var.body_bytes_sent .. ',' ..
+        '\"request\":\"'         .. ngx.var.request .. ',' ..
+        '\"http_user_agent\":\"' .. transform(ngx.var.http_user_agent) .. ',' ..
+        '\"request_time\":\"'    .. ngx.var.request_time .. ',' ..
+        '\"http_referer\":\"'    .. transform(ngx.var.http_referer) .. ',' ..
+        '\"http_x_forwarded_for\":\"' .. transform(ngx.var.http_x_forwarded_for) .. ',' ..
+        '\"server_name\":\"'     .. ngx.var.server_name .. ',' ..
+        '\"bytes_sent\":\"'      .. ngx.var.bytes_sent .. '}' .. '\n'
 
     return msg
 
@@ -25,11 +35,12 @@ end
 
 local function write_file( data )
 
-    local f, err_code, err_msg = file_ffi:open(TEST_FILE_PATH,
-                                               bit.bor(file_ffi.O_CREAT,
-                                                       file_ffi.O_RDWR,
-                                                       file_ffi.O_TRUNC),
-                                                file_ffi.S_IRWXU)
+    local file_path = '/opt/qq-openresty/nginx/logs/mou.log'
+    local flag = bit.bor(file_ffi.O_CREAT, file_ffi.O_WRONLY, file_ffi.O_APPEND)
+    local mode = bit.bor(file_ffi.S_IRUSR, file_ffi.S_IWUSR,
+                         file_ffi.S_IRGRP, file_ffi.S_IROTH)
+
+    local f, err_code, err_msg = file_ffi:open(file_path, flag, mode)
     if err_code ~= nil then
         return nil, err_code, err_msg
     end
@@ -44,19 +55,21 @@ local function write_file( data )
         return nil, err_code, err_msg
     end
 
+    return nil, nil, nil
 end
-
 
 function _M.logger()
 
+    ngx.log(ngx.ERR, 'logger begin ')
     local msg = access_log()
-    local res, err_code, er err_msg = write_file( msg )
+    local res, err_code, err_msg = write_file( msg )
 
     if err_code ~= nil then
-        ngx.log('logger failed! err_code: ', err_code, ' err_msg: ', err_msg)
+        ngx.log(ngx.ERR, 'logger failed! err_code: ', err_code, ' err_msg: ', err_msg)
     end
 
 end
 
+return _M
 
 
