e0fda37d4e59bf910967c7dc9de667b94533d7d6
xiaoxuanzi <xiaoximou@gmail.com>
2017-04-12 15:51:02 +0800

    fix code

--- logger.lua
+++ logger.lua
@@ -1,4 +1,4 @@
-local file_ffi = require "std_ffi.file"
+local file_ffi = require "logger.file"
 
 local _M = {}
 
@@ -6,32 +6,32 @@ local function access_log()
 
     local msg = {}
 
-    table.insert(msg, '{')
+    table.insert(msg, '{ ')
     table.insert(msg, '\"timestamp\":\"')
-    table.insert(msg, ngx.var.time_local)
-    table.insert(msg, ', \"remote_addr\":\"')
-    table.insert(msg, ngx.var.remote_addr or '-\"')
-    table.insert(msg, ', \"remote_user\":\"')
-    table.insert(msg, ngx.var.remote_user or '-\"')
-    table.insert(msg, ', \"upstream_addr\":\"')
-    table.insert(msg, ngx.var.upstream_addr or '-\"')
-    table.insert(msg, ', \"status\":\"')
+    table.insert(msg, ngx.var.time_iso8601)
+    table.insert(msg, '\",\"remote_addr\":\"')
+    table.insert(msg, ngx.var.remote_addr or '-')
+    table.insert(msg, '\",\"remote_user\":\"')
+    table.insert(msg, ngx.var.remote_user or '-')
+    table.insert(msg, '\",\"upstream_addr\":\"')
+    table.insert(msg, ngx.var.upstream_addr or '-')
+    table.insert(msg, '\",\"status\":\"')
     table.insert(msg, ngx.var.status)
-    table.insert(msg, ', \"body_bytes_sent\":\"')
+    table.insert(msg, '\",\"body_bytes_sent\":')
     table.insert(msg, ngx.var.body_bytes_sent)
-    table.insert(msg, ', \"request\":\"')
+    table.insert(msg, ',\"request\":\"')
     table.insert(msg, ngx.var.request)
-    table.insert(msg, ', \"http_user_agent\":\"')
-    table.insert(msg, ngx.var.http_user_agent or '-\"')
-    table.insert(msg, ', \"request_time\":\"')
+    table.insert(msg, '\",\"http_user_agent\":\"')
+    table.insert(msg, ngx.var.http_user_agent or '-')
+    table.insert(msg, '\",\"request_time\":')
     table.insert(msg, ngx.var.request_time)
-    table.insert(msg, ', \"http_referer\":\"')
-    table.insert(msg, ngx.var.http_referer or '-\"')
-    table.insert(msg, ', \"http_x_forwarded_for\":\"')
-    table.insert(msg, ngx.var.http_x_forwarded_for or '-\"')
-    table.insert(msg, ', \"server_name\":\"')
+    table.insert(msg, ',\"http_referer\":\"')
+    table.insert(msg, ngx.var.http_referer or '-')
+    table.insert(msg, '\",\"http_x_forwarded_for\":\"')
+    table.insert(msg, ngx.var.http_x_forwarded_for or '-')
+    table.insert(msg, '\",\"server_name\":\"')
     table.insert(msg, ngx.var.server_name)
-    table.insert(msg, ', \"bytes_sent\":\"')
+    table.insert(msg, '\",\"bytes_sent\":\"')
     table.insert(msg, ngx.var.bytes_sent)
     table.insert(msg, '\"}')
     table.insert(msg, '\n')
@@ -41,9 +41,8 @@ local function access_log()
 
 end
 
-local function write_file( data )
+local function write_file( data, file_path )
 
-    local file_path = '/opt/qq-openresty/nginx/logs/mou.log'
     local flag = bit.bor(file_ffi.O_CREAT, file_ffi.O_WRONLY, file_ffi.O_APPEND)
     local mode = bit.bor(file_ffi.S_IRUSR, file_ffi.S_IWUSR,
                          file_ffi.S_IRGRP, file_ffi.S_IROTH)
@@ -66,11 +65,13 @@ local function write_file( data )
     return nil, nil, nil
 end
 
-function _M.logger()
+function _M.logger( file_path )
 
-    ngx.log(ngx.ERR, 'logger begin ')
+
+    assert(file_path != nil)
+
     local msg = access_log()
-    local res, err_code, err_msg = write_file( msg )
+    local res, err_code, err_msg = write_file( msg, file_path )
 
     if err_code ~= nil then
         ngx.log(ngx.ERR, 'logger failed! err_code: ', err_code, ' err_msg: ', err_msg)
