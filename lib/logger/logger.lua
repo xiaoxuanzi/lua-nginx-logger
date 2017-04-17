50cf154fde032610654a041d1f38373f8b4c2075
xiaoxuanzi <xiaoximou@gmail.com>
2017-04-17 10:38:38 +0800

    add status

--- lib/logger/logger.lua
+++ lib/logger/logger.lua
@@ -1,6 +1,22 @@
 local file_ffi = require "logger.file"
+local json     = require( "cjson" )
+
+local logger_key = 'logger-status'
+local store_data = ngx.shared.logger_status
+local INIT_VAL   = 'MRD_INTIT_VAL'
 
 local _M = {}
+
+function _M.init()
+
+    local val = INIT_VAL
+    local succ, err, f = store_data:set(logger_key, val)
+    if not succ then
+        ngx.log(ngx.ERR, 'init store data failed, err: ' .. err)
+        error('Init failed. error: ' .. err .. ' aborting!!')
+    end
+
+end
 
 local function access_log()
 
@@ -65,6 +81,44 @@ local function write_file( data, file_path )
     return nil, nil, nil
 end
 
+local function get_dict_data( dict, key )
+
+    local val, err = dict:get( key )
+
+    if not val then
+        return nil, err
+    end
+
+    if val == INIT_VAL then
+        return val, nil
+    end
+
+    local err, data = pcall( json.decode, val )
+    if not err then
+        ngx.log(ngx.ERR, 'get logger status failed, err : ' .. err)
+
+        return nil, err
+    end
+
+    return data, nil
+
+end
+
+local function set_dict_data( dict, key, data )
+
+    local err, json_data = pcall(json.encode, data )
+    if not err then
+        ngx.log(ngx.ERR, 'set dict data faled, since json decode, err: ', err)
+        return
+    end
+
+    local succ, err, f = dict:set(key, json_data )
+    if not succ then
+        ngx.log(ngx.ERR, 'set dict data failed, err: ' .. err)
+    end
+
+end
+
 function _M.logger( file_path )
 
 
@@ -73,9 +127,75 @@ function _M.logger( file_path )
     local msg = access_log()
     local res, err_code, err_msg = write_file( msg, file_path )
 
-    if err_code ~= nil then
-        ngx.log(ngx.ERR, 'logger failed! err_code: ', err_code, ' err_msg: ', err_msg)
+    if err_code == nil then
+        return
     end
+
+    ngx.log(ngx.ERR, 'logger failed! err_code: ', err_code, ' err_msg: ', err_msg)
+    --record err msg
+
+    local data, err = get_dict_data( store_data, logger_key )
+    if not data then
+        ngx.log(ngx.ERR, 'get dict data failed, err: '.. err)
+        return
+    end
+
+    if data == INIT_VAL then
+        data = {}
+    end
+
+    local skey = ngx.var.upstream_addr
+    if not data[ skey ] then
+        data[ skey ] = {
+            total = 0,
+            err_code = '',
+            err_msg = ''
+        }
+    end
+
+    data[ skey ][ total ] = data[ skey ][ total ] + 1
+    data[ skey ][ err_code ] = err_code
+    data[ skey ][ err_msg ]  = err_msg
+
+    set_dict_data( store_data, logger_key, data )
+
+end
+
+function _M.logger_status()
+
+    local _ret = {
+
+        nginx_info = {
+            nginx_version = ngx.var.nginx_version,
+            address = ngx.var.server_addr,
+            timestamp = ngx.now() * 1000,
+            time_iso8601 = ngx.var.time_iso8601,
+            pid = ngx.worker.pid()
+        },
+
+        status = {}
+
+    }
+
+    local logger_status, err = get_dict_data(store_data, logger_key)
+    if not logger_status then
+        ngx.log(ngx.ERR, 'get statistic failed, err: ' .. err)
+        ngx.exit(500)
+    end
+
+    if logger_status == INIT_VAL then
+
+        ngx.print( 'There is no data yet, please try later!' )
+        ngx.exit(ngx.HTTP_OK)
+
+    end
+
+    _ret.status = logger_status
+
+    local ret = json.encode( _ret )
+    ngx.status = ngx.HTTP_OK
+    ngx.print( ret )
+    ngx.exit(ngx.HTTP_OK)
 
 end
 
